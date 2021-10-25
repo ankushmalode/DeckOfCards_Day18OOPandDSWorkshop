af7e1f3e31edc95458b74de6467f38f5e656c4d2
ankushmalode <you@example.com>
2021-10-25 13:22:18 +0530

    Setup the initial game with deck of cards

--- src/main/java/com/bridgelabz/DeckOfCards.java
+++ src/main/java/com/bridgelabz/DeckOfCards.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class DeckOfCards {
     public static void main(String[] args) {
         System.out.println("Welcome to the Card Games");
+        UniqueCards.distribute();
+        UniqueCards.display();
     }
 }
